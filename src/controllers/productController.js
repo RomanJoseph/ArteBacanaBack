7b1e7eabef980c0143b9eb3a4a37c91678eafb16
Shizuke136 <romanjose3008@gmail.com>
2022-09-19 01:36:16 -0300

    getProduct by Id done

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,4 +1,5 @@
 import { db } from "../db/db.js"
+import { ObjectId } from "mongodb"
 
 async function listProducts(req, res) {
     try {
@@ -18,7 +19,7 @@ async function sendProduct(req, res) {
     const { name, seller, stock, type, image, price, description } = req.body
 
     try{
-        db.collection("products").insertOne({name, seller, stock, type, image, price, description})
+        await db.collection("products").insertOne({name, seller, stock, type, image, price, description})
     }catch(error){
         return console.log(error)
     }
@@ -26,4 +27,20 @@ async function sendProduct(req, res) {
     return res.sendStatus(201)
 }
 
-export { listProducts, sendProduct }
+async function getProduct(req, res){
+    const { id } = req.body
+    const productId = ObjectId(id)
+
+    if(!(await db.collection("products").findOne({_id : productId}))){
+        return res.sendStatus(404)
+    }
+
+    try{
+        const product = await db.collection("products").findOne({_id : productId})
+        return res.send(product)
+    }catch(error){
+        return console.log(error)
+    }
+}
+
+export { listProducts, sendProduct, getProduct }
--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -1,10 +1,11 @@
 import express from "express";
 
-import { listProducts, sendProduct } from "../controllers/productController.js";
+import { getProduct, listProducts, sendProduct } from "../controllers/productController.js";
 
 const router = express.Router();
 
 router.get("/products", listProducts);
-router.post("/products", sendProduct)
+router.post("/products", sendProduct);
+router.get("/product", getProduct)
 
 export default router;
